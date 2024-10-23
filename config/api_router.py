edaec491f5387d306d5463bd570ce750da86762c
Raphael Miranda <raphael.miranda75@gmail.com>
2024-10-23 14:14:21 -0300

    refactor: Sale Viewset

--- config/api_router.py
+++ config/api_router.py
@@ -6,6 +6,7 @@ from corpsystem_teste.app.modules.users.api.views import UserViewSet
 from corpsystem_teste.app.modules.clients.api.views import ClientsViewSet
 from corpsystem_teste.app.modules.sellers.api.views import SellersViewSet
 from corpsystem_teste.app.modules.products.api.views import ProductsViewSet
+from corpsystem_teste.app.modules.sales.api.views import SalesViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
@@ -13,6 +14,7 @@ router.register("users", UserViewSet, basename="users")
 router.register("products", ProductsViewSet, basename="products")
 router.register("clients", ClientsViewSet, basename="clients")
 router.register("sellers", SellersViewSet, basename="sellers")
+router.register("sales", SalesViewSet, basename="sales")
 
 
 app_name = "api"
--- corpsystem_teste/app/modules/sales/api/views.py
+++ corpsystem_teste/app/modules/sales/api/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from corpsystem_teste.app.mixins.viewsets import BaseView
 from corpsystem_teste.app.modules.sales.models import Sale
 from corpsystem_teste.app.modules.products.models import Product
+from corpsystem_teste.app.modules.sellers.models import Seller
+from corpsystem_teste.app.modules.clients.models import Client
 
 from .serializers import (
     SaleListSerializer,
@@ -27,14 +29,34 @@ class SalesViewSet(SalesActions, BaseView):
 
     def get_queryset(self) -> List[Sale]:
         return Sale.objects.all()
+
+    def validate(self, data: dict) -> dict:
+        seller = Seller.objects.filter(pk=data.get('seller'))
+        client = Client.objects.filter(pk=data.get('client'))
+        product = Product.objects.filter(pk=data.get('product'))
+
+        if not seller:
+            return {'message': 'Vendedor não encontrado'}
+        if not client:
+            return {'message': 'Cliente não encontrado'}
+        if not product:
+            return {'message': 'Produto não encontrado'}
+
+        return data
     
     def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        product = Product.objects.get(pk=request.data.get('product'))
-        if product.quantity < request.data.get('quantity'):
-            return Response({'message': 'Quantidade maior que o estoque'}, status=400)
-        product.quantity -= request.data.get('quantity')
-        product.save()
-        request.data._mutable = True
-        request.data['total_price'] = product.price * request.data.get('quantity')
-        request.data._mutable = False
-        return super().create(request, *args, **kwargs)
+        try:
+            data = self.validate(request.data)
+            if data.get('message'):
+                return Response(data, status=400)
+            product = Product.objects.get(pk=data.get('product'))
+            if product.quantity < request.data.get('quantity'):
+                return Response({'message': 'Quantidade maior que o estoque'}, status=400)
+            product.quantity -= request.data.get('quantity')
+            product.save()
+            request.data._mutable = True
+            request.data['total_price'] = product.price * request.data.get('quantity')
+            request.data._mutable = False
+            return super().create(request, *args, **kwargs)
+        except Product.DoesNotExist:
+            return Response({'message': 'Produto não encontrado'}, status=400)
