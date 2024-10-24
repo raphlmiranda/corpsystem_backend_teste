cff47acf4cc8a786ef2ed9c9499cf3d70a55dd31
Raphael Miranda <wavecode@Mac-mini-de-wavecode.local>
2024-10-24 03:43:00 -0300

    fix: permission

--- corpsystem_teste/app/modules/sales/api/views.py
+++ corpsystem_teste/app/modules/sales/api/views.py
@@ -1,7 +1,7 @@
 from typing import Any, List
 from rest_framework import status
 from django.http import HttpResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -25,6 +25,7 @@ from corpsystem_teste.app.modules.sales.utils.export import (
 
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def export_file(request) -> HttpResponse:
 
     valid_extensions = ['pdf', 'xlsx']
